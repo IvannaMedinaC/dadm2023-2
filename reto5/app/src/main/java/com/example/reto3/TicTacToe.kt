7f1571acf147f5da86bc975f908165dc4251ca2c
IvannaMedinaC <imedinac@unal.edu.co>
2023-10-01 10:21:52 -0500

    create reto5

---
+++ reto5/app/src/main/java/com/example/reto3/TicTacToe.kt
@@ -0,0 +1,154 @@
+package com.example.reto3
+import java.util.Random
+
+
+public class TicTacToe{
+    val mBoard = charArrayOf('1', '2', '3', '4', '5', '6', '7', '8', '9')
+    private var testBoard = charArrayOf('1', '2', '3', '4', '5', '6', '7', '8', '9')
+    val BOARD_SIZE = 9
+    private val mRand: Random
+    // The computer's difficulty levels
+    enum class DifficultyLevel { Easy, Harder, Expert };
+    // Current difficulty level
+    private var mDifficultyLevel = DifficultyLevel.Expert
+
+    init {
+        // Seed the random number generator
+        mRand = Random()
+        var turn = HUMAN_PLAYER // Human starts first
+        var win = 0 // Set to 1, 2, or 3 when game is over
+
+        // Keep looping until someone wins or a tie
+    }
+
+    fun getDifficultyLevel(): DifficultyLevel? {
+        return mDifficultyLevel
+    }
+    fun setDifficultyLevel(difficultyLevel: DifficultyLevel) {
+        mDifficultyLevel = difficultyLevel
+    }
+
+    public fun clearBoard() {
+        for (i in 0 until BOARD_SIZE){
+            mBoard[i]= OPEN_SPOT
+        }
+    }
+
+    //Pone la x o la o en la casilla dada
+    public fun setMove (player: Char, location: Int) {
+        if(mBoard[location]== OPEN_SPOT){
+            mBoard[location] = player
+        }
+    }
+
+    public fun checkForWinner(): Int {
+
+        // Check horizontal wins
+        for (i in 0..6 step 3) {
+            if (mBoard[i] == HUMAN_PLAYER && mBoard[i + 1] == HUMAN_PLAYER && mBoard[i + 2] == HUMAN_PLAYER) return 2
+            if (mBoard[i] == COMPUTER_PLAYER && mBoard[i + 1] == COMPUTER_PLAYER && mBoard[i + 2] == COMPUTER_PLAYER) return 3
+        }
+
+        // Check vertical wins
+        for (i in 0..2) {
+            if (mBoard[i] == HUMAN_PLAYER && mBoard[i + 3] == HUMAN_PLAYER && mBoard[i + 6] == HUMAN_PLAYER) return 2
+            if (mBoard[i] == COMPUTER_PLAYER && mBoard[i + 3] == COMPUTER_PLAYER && mBoard[i + 6] == COMPUTER_PLAYER) return 3
+        }
+
+        // Check for diagonal wins
+        if (mBoard[0] == HUMAN_PLAYER && mBoard[4] == HUMAN_PLAYER && mBoard[8] == HUMAN_PLAYER || mBoard[2] == HUMAN_PLAYER && mBoard[4] == HUMAN_PLAYER && mBoard[6] == HUMAN_PLAYER) return 2
+        if (mBoard[0] == COMPUTER_PLAYER && mBoard[4] == COMPUTER_PLAYER && mBoard[8] == COMPUTER_PLAYER || mBoard[2] == COMPUTER_PLAYER && mBoard[4] == COMPUTER_PLAYER && mBoard[6] == COMPUTER_PLAYER) return 3
+
+        // Check for tie
+        for (i in 0 until BOARD_SIZE) {
+            // If we find a number, then no one has won yet
+            if (mBoard[i] != HUMAN_PLAYER && mBoard[i] != COMPUTER_PLAYER) return 0
+        }
+
+        // If we make it through the previous loop, all places are taken, so it's a tie
+        return 1
+    }
+
+    fun getcomputerMove(): Int{
+        var move: Int
+        move = -1
+
+        if(mDifficultyLevel == DifficultyLevel.Easy)
+            move = getRandomMove();
+
+        else if(mDifficultyLevel == DifficultyLevel.Harder){
+            move = getWinningMove()
+            if(move == -1){
+                move = getRandomMove()
+            }
+        }
+        else if(mDifficultyLevel == DifficultyLevel.Expert){
+            move = getWinningMove()
+            if(move == -1){
+                move = getBlockingMove()
+            }
+            if(move == -1){
+                move = getRandomMove()
+            }
+        }
+        return move
+    }
+
+    fun getRandomMove():Int{
+        // Generate random move
+        var move: Int
+        do {
+            move = mRand.nextInt(BOARD_SIZE)
+        } while (mBoard[move] == HUMAN_PLAYER || mBoard[move] == COMPUTER_PLAYER)
+        mBoard[move] = COMPUTER_PLAYER
+        return move
+    }
+    fun getWinningMove():Int{
+        // First see if there's a move O can make to win
+        var move: Int
+        move = -1
+        for (i in 0 until BOARD_SIZE) {
+            if (mBoard[i] != HUMAN_PLAYER && mBoard[i] != COMPUTER_PLAYER) {
+                val curr = mBoard[i]
+                mBoard[i] = COMPUTER_PLAYER
+                if (checkForWinner() == 3) {
+                    move = i
+                    break
+                }
+                else mBoard[i] = curr
+            }
+        }
+        return move
+    }
+    fun getBlockingMove():Int{
+        var move:Int
+        move = -1
+        // See if there's a move O can make to block X from winning
+        for (i in 0 until BOARD_SIZE) {
+            if (mBoard[i] != HUMAN_PLAYER && mBoard[i] != COMPUTER_PLAYER) {
+                val curr = mBoard[i] // Save the current number
+                mBoard[i] = HUMAN_PLAYER
+                if (checkForWinner() == 2) {
+                    mBoard[i] = COMPUTER_PLAYER
+                    move = i
+                    break
+                } else mBoard[i] = curr
+            }
+        }
+        return move
+    }
+
+    companion object {
+        const val HUMAN_PLAYER = 'X'
+        const val COMPUTER_PLAYER = 'O'
+        const val OPEN_SPOT = ' '
+        const val SIZE_B = 9
+        /**
+         * @param args
+         */
+        @JvmStatic
+        fun main(args: Array<String>) {
+            TicTacToe()
+        }
+    }
+}
---
+++ reto5/app/src/main/java/com/example/reto3/TicTacToe_Activity.kt
@@ -0,0 +1,196 @@
+package com.example.reto3
+
+import android.app.Dialog
+import android.graphics.Color
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import android.app.AlertDialog;
+import android.widget.Toast
+import android.content.DialogInterface
+
+
+public class TicTacToe_Activity :AppCompatActivity() {
+
+    private lateinit var mGame: TicTacToe
+
+    // Buttons making up the board
+    private lateinit var mBoardButtons: ArrayList<Button>
+
+    // Various text displayed
+    private lateinit var mInfoTextView: TextView
+    private lateinit var newButton: Button
+    val DIALOG_DIFFICULTY_ID = 0
+    val DIALOG_QUIT_ID = 1
+
+    @Override
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ui_tic_tac_toe)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        mGame = TicTacToe()
+        //newButton = findViewById(R.id.button)
+        mBoardButtons = arrayListOf<Button>()
+        mBoardButtons.add(findViewById<Button>(R.id.one))
+        mBoardButtons.add(findViewById<Button>(R.id.two))
+        mBoardButtons.add(findViewById<Button>(R.id.three))
+        mBoardButtons.add(findViewById<Button>(R.id.four))
+        mBoardButtons.add(findViewById<Button>(R.id.five))
+        mBoardButtons.add(findViewById<Button>(R.id.six))
+        mBoardButtons.add(findViewById<Button>(R.id.seven))
+        mBoardButtons.add(findViewById<Button>(R.id.eigth))
+        mBoardButtons.add(findViewById<Button>(R.id.nine))
+        mInfoTextView = findViewById(R.id.information) as TextView
+
+        startNewGame()
+    }
+
+    private fun startNewGame() {
+        mGame.clearBoard()
+        // Reset all buttons
+        for (i in 0 until mBoardButtons.size) {
+            mBoardButtons[i]?.text = ""
+            mBoardButtons[i]?.isEnabled = true
+            mBoardButtons[i]?.setOnClickListener(ButtonClickListener(i))
+
+        }
+        // Human goes first
+        mInfoTextView.setText("You go first.");
+        /*newButton.setOnClickListener { mGame.clearBoard()
+            for (i in 0 until mBoardButtons.size) {
+            mBoardButtons[i]?.text = ""
+            mBoardButtons[i]?.isEnabled = true
+            mBoardButtons[i]?.setOnClickListener(ButtonClickListener(i))
+
+        }
+        }*/
+    }
+
+    inner private class ButtonClickListener(var location: Int) : View.OnClickListener {
+        override fun onClick(view: View) {
+            if (mBoardButtons!!.get(location)!!.isEnabled()) {
+                setMove(TicTacToe.HUMAN_PLAYER, location)
+                // If no winner yet, let the computer make a move
+                var winner: Int = 0
+                winner = mGame.checkForWinner()
+                if (winner == 0) {
+                    mInfoTextView.setText("It's Android's turn.")
+                    val move: Int = mGame.getcomputerMove()
+                    setMove(TicTacToe.COMPUTER_PLAYER, move)
+                    winner = mGame.checkForWinner()
+                }
+                if (winner == 0) {
+                    mInfoTextView.setText("It's your turn.")
+                } else if (winner == 1) {
+                    mInfoTextView.setText("It's a tie!")
+                    for (i in 0 until mBoardButtons.size) {
+                        mBoardButtons[i].isEnabled = false
+                    }
+                } else if (winner == 2) {
+                    mInfoTextView.setText("You won!")
+                    for (i in 0 until mBoardButtons.size) {
+                        mBoardButtons[i].isEnabled = false
+                    }
+                } else if (winner == 3) {
+                    mInfoTextView.setText("Android won!")
+                    for (i in 0 until mBoardButtons.size) {
+                        mBoardButtons[i].isEnabled = false
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setMove(player: Char, location: Int) {
+        mGame.setMove(player, location)
+        mBoardButtons[location]?.isEnabled = false
+        mBoardButtons[location]?.text = player.toString()
+        if (player == TicTacToe.HUMAN_PLAYER) mBoardButtons[location]?.setTextColor(
+            Color.rgb(
+                0,
+                200,
+                0
+            )
+        ) else mBoardButtons[location]?.setTextColor(Color.rgb(200, 0, 0))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        val inflater = menuInflater
+        inflater.inflate(R.menu.tictactoe_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.new_game -> {
+                startNewGame()
+                return true
+            }
+
+            R.id.ai_difficulty -> {
+                showDialog(DIALOG_DIFFICULTY_ID)
+                return true
+            }
+
+            R.id.quit -> {
+                showDialog(DIALOG_QUIT_ID)
+                return true
+            }
+        }
+        return false
+    }
+
+    protected override fun onCreateDialog(id: Int): Dialog? {
+        var dialog: Dialog? = null
+        val builder = AlertDialog.Builder(this)
+
+        when (id) {
+            DIALOG_DIFFICULTY_ID -> {
+                builder.setTitle(R.string.difficulty_choose)
+                val levels = arrayOf(
+                    getString(R.string.difficulty_easy),
+                    getString(R.string.difficulty_harder),
+                    getString(R.string.difficulty_expert)
+                )
+                var selected: Int = 2
+                builder.setSingleChoiceItems(levels, selected) { dialog, item ->
+                    dialog.dismiss()
+                    if (item == 0) {
+                        mGame.setDifficultyLevel(TicTacToe.DifficultyLevel.Easy)
+                    } else if (item == 1) {
+                        mGame.setDifficultyLevel(TicTacToe.DifficultyLevel.Harder)
+                    } else if (item == 2) {
+                        mGame.setDifficultyLevel(TicTacToe.DifficultyLevel.Expert)
+                    }
+
+                    Toast.makeText(applicationContext, levels[item], Toast.LENGTH_SHORT).show()
+                }
+
+                dialog = builder.create()
+            }
+
+            DIALOG_QUIT_ID ->{
+                builder.setMessage(R.string.quit_question)
+                builder.setCancelable(false)
+                builder.setPositiveButton(R.string.yes) { dialog, id ->
+                    this@TicTacToe_Activity.finish()
+                }
+                builder.setNegativeButton(R.string.no, null)
+                dialog = builder.create()
+            }
+        }
+        return dialog!!
+    }
+
+}
+
+
